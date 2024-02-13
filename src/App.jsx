9da0082ad11eb14699dc77d678e3b9ac54ea8a82
osama <osamaaslam029@gmail.com>
2024-02-13 19:27:45 +0500

    Product Table

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// Components :
+import ProductTable from './components/ProductTable'
+
+// CSS :
+import './App.css'
+
+
+function App() {
+
+  return (<ProductTable />)
+}
+
+export default App
---
+++ src/constants/prodList.js
@@ -0,0 +1,7 @@
+export const prodList = [
+    { id: 1, name: "Gaming merchandise" },
+    { id: 2, name: "Shop activity trackers and smartwatches" },
+    { id: 3, name: "Player's paradise starts here" },
+    { id: 4, name: "A whole new way to work" },
+    { id: 5, name: "Best Sellers in Books" },
+]
