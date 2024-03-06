fcbf311786b5e5b55f8c76c4839a2d81f09d6fcf
osama <osamaaslam029@gmail.com>
2024-03-06 16:49:38 +0500

    add conso

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,102 +1,126 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // ANT-D :
-import { Table } from 'antd';
+import { Table } from "antd";
 
 // DND :
-import { DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core';
-import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
 import {
-    SortableContext,
-    arrayMove,
-    useSortable,
-    verticalListSortingStrategy,
-} from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
+  DndContext,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import {
+  SortableContext,
+  arrayMove,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 // Constant :
-import { prodList } from '../constants/prodList';
-
+import { prodList } from "../constants/prodList";
 
 const columns = [
-    {
-        title: 'No.',
-        dataIndex: 'index',
-        render: (text, record, index) => index + 1,
-    },
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        sorter: (a, b) => a.name.length - b.name.length,
-    },
+  {
+    title: "No.",
+    dataIndex: "index",
+    render: (text, record, index) => index + 1,
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    sorter: (a, b) => a.name.length - b.name.length,
+  },
 ];
 
 const Row = (props) => {
-    const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
-        id: props['data-row-key'],
-    });
-    const style = {
-        ...props.style,
-        transform: CSS.Transform.toString(
-            transform && {
-                ...transform,
-                scaleY: 1,
-            },
-        ),
-        transition,
-        cursor: 'move',
-        ...(isDragging
-            ? {
-                position: 'relative',
-                zIndex: 9999,
-            }
-            : {}),
-    };
-    return <tr {...props} ref={setNodeRef} style={style} {...attributes} {...listeners} />;
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: props["data-row-key"],
+  });
+  const style = {
+    ...props.style,
+    transform: CSS.Transform.toString(
+      transform && {
+        ...transform,
+        scaleY: 1,
+      }
+    ),
+    transition,
+    cursor: "move",
+    ...(isDragging
+      ? {
+          position: "relative",
+          zIndex: 9999,
+        }
+      : {}),
+  };
+  return (
+    <tr
+      {...props}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    />
+  );
 };
 
 const ProductTable = () => {
-    // List of products 
-    const [prodListArray, setProdListArray] = useState(prodList)
+  // List of products
+  const [prodListArray, setProdListArray] = useState(prodList);
 
-    const sensors = useSensors(
-        useSensor(PointerSensor, {
-            activationConstraint: {
-                distance: 1,
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 1,
+      },
+    })
+  );
+
+  console.log("--------------->");
+  const onDragEnd = ({ active, over }) => {
+    if (active.id !== over?.id) {
+      setProdListArray((prev) => {
+        const activeIndex = prev.findIndex((i) => i.id === active.id);
+        const overIndex = prev.findIndex((i) => i.id === over?.id);
+        return arrayMove(prev, activeIndex, overIndex);
+      });
+    }
+  };
+
+  console.log("------------Updated List", prodListArray);
+  return (
+    <DndContext
+      sensors={sensors}
+      modifiers={[restrictToVerticalAxis]}
+      onDragEnd={onDragEnd}
+    >
+      <SortableContext
+        items={prodListArray.map((i) => i.id)}
+        strategy={verticalListSortingStrategy}
+      >
+        <Table
+          components={{
+            body: {
+              row: Row,
             },
-        }),
-    );
-
-    const onDragEnd = ({ active, over }) => {
-        if (active.id !== over?.id) {
-            setProdListArray((prev) => {
-                const activeIndex = prev.findIndex((i) => i.id === active.id);
-                const overIndex = prev.findIndex((i) => i.id === over?.id);
-                return arrayMove(prev, activeIndex, overIndex);
-            });
-        }
-    };
-
-    console.log("------------Updated List", prodListArray)
-    return (
-        <DndContext sensors={sensors} modifiers={[restrictToVerticalAxis]} onDragEnd={onDragEnd}>
-            <SortableContext
-                items={prodListArray.map((i) => i.id)}
-                strategy={verticalListSortingStrategy}
-            >
-                <Table
-                    components={{
-                        body: {
-                            row: Row,
-                        },
-                    }}
-                    rowKey="id"
-                    columns={columns}
-                    dataSource={prodListArray}
-                    pagination={false}
-                />
-            </SortableContext>
-        </DndContext>
-    );
+          }}
+          rowKey="id"
+          columns={columns}
+          dataSource={prodListArray}
+          pagination={false}
+        />
+      </SortableContext>
+    </DndContext>
+  );
 };
 export default ProductTable;
